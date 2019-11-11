93db23918b379ec2671cd03c61b13ee4be1810db
fredrik berntsson <fredrikberntsson@hotmail.com>
2019-11-11 19:41:25 +0100

    inlämningsuppgift javascript

--- inlämningsuppgift_javascript/js/clearField.js
+++
@@ -1,7 +0,0 @@
-const clearField = () => {
-  let delBtn = document.getElementById("eraseBtn");
-  let userSearch = (document.getElementById("searchInput").value = "");
-  delBtn.style.display = "none";
-};
-
-export default clearField;
